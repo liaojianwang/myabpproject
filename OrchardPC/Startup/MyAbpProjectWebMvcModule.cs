9f90e5e85c9aa629670b2092ebac2cde2c169a49
廖建旺 <liaojianwang@hotmail.com>
2019-11-14 22:31:44 +0800

    站点启动成功

---
+++ OrchardPC/Startup/MyAbpProjectWebMvcModule.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using MyAbpProject.Configuration;
+using MyAbpProject;
+
+namespace OrchardPC.Startup
+{
+    [DependsOn(typeof(MyAbpProjectWebCoreModule))]
+    public class MyAbpProjectWebMvcModule : AbpModule
+    {
+        private readonly IHostingEnvironment _env;
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public MyAbpProjectWebMvcModule(IHostingEnvironment env)
+        {
+            _env = env;
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        public override void PreInitialize()
+        {
+            //Configuration.Navigation.Providers.Add<MyAbpProjectNavigationProvider>();
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(MyAbpProjectWebMvcModule).GetAssembly());
+        }
+    }
+}
---
+++ OrchardPC/Startup/Startup.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.AspNetCore;
+using Abp.AspNetCore.Mvc.Antiforgery;
+using Abp.Castle.Logging.Log4Net;
+using Abp.Dependency;
+using Abp.Json;
+using Castle.Core.Logging;
+using Castle.Facilities.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+using MyAbpProject.Configuration;
+using Newtonsoft.Json.Serialization;
+
+namespace OrchardPC.Startup
+{
+    public class Startup
+    {
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        //public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            //services.AddControllersWithViews();
+            // MVC
+            services.AddControllersWithViews(
+                options =>
+                {
+                    options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+                    options.Filters.Add(new AbpAutoValidateAntiforgeryTokenAttribute());
+                }
+            ).AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ContractResolver = new AbpMvcContractResolver(IocManager.Instance)
+                {
+                    NamingStrategy = new CamelCaseNamingStrategy()
+                };
+            });
+            // Configure Abp and Dependency Injection
+            return services.AddAbp<MyAbpProjectWebMvcModule>(
+                // Configure Log4Net logging
+                options => options.IocManager.IocContainer.AddFacility<LoggingFacility>(
+                    f => f.UseAbpLog4Net().WithConfig("log4net.config")
+                )
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseAbp(); // Initializes ABP framework.
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
