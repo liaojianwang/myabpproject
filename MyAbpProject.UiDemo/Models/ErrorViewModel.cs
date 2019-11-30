3d7742f26eef7d67b5e60b73661b51d69aec819c
liaojianwang <liaojianwang@qibingdaojia.com>
2019-11-30 13:35:08 +0800

    新增了一个Web项目。

---
+++ MyAbpProject.UiDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyAbpProject.UiDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
