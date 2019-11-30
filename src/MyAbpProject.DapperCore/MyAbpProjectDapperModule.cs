1da352c31e6a61748c02a08259ea53901ae4afab
liaojianwang <liaojianwang@qibingdaojia.com>
2019-11-30 13:58:20 +0800

    使用Dapper模块

---
+++ src/MyAbpProject.DapperCore/MyAbpProjectDapperModule.cs
@@ -0,0 +1,19 @@
+﻿using Abp.Dapper;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace MyAbpProject.DapperCore
+{
+    [DependsOn(typeof(MyAbpProjectCoreModule), typeof(AbpDapperModule))]
+    public class MyAbpProjectDapperModule : AbpModule
+    {
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+            DapperExtensions.DapperExtensions.SetMappingAssemblies(new List<Assembly> { typeof(MyAbpProjectDapperModule).GetAssembly() });
+        }
+    }
+}
