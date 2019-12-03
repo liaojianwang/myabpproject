d260b7be446388df89ec1ce21edbc0082c219891
liaojianwang <liaojianwang@qibingdaojia.com>
2019-12-03 15:39:07 +0800

    新增MyAbpProjectWebMvcModule模块。

---
+++ MyAbpProject.DcTms/Startup/MyAbpProjectWebMvcModule.cs
@@ -0,0 +1,36 @@
+﻿using Abp.Modules;
+using Abp.Reflection.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using MyAbpProject.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAbpProject.DcTms.Startup
+{
+    [DependsOn(typeof(MyAbpProjectWebCoreModule))]
+    public class MyAbpProjectWebMvcModule : AbpModule
+    {
+        [Obsolete]
+        private readonly IHostingEnvironment _env;
+        private readonly IConfigurationRoot _appConfiguration;
+
+        [Obsolete]
+        public MyAbpProjectWebMvcModule(IHostingEnvironment env)
+        {
+            _env = env;
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        public override void PreInitialize()
+        {
+
+        }
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(MyAbpProjectWebMvcModule).GetAssembly());
+        }
+    }
+}
--- MyAbpProject.DcTms/Startup/Program.cs
+++ MyAbpProject.DcTms/Startup/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace MyAbpProject.DcTms
+namespace MyAbpProject.DcTms.Startup
 {
     public class Program
     {
