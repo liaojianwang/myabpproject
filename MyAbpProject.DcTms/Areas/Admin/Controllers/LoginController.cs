b0ba508ffd7befbe6f9cfead0cf2b1a54227e4b3
liaojianwang <liaojianwang@qibingdaojia.com>
2019-12-09 18:03:15 +0800

    新增登录页

--- MyAbpProject.DcTms/Areas/Admin/Controllers/LoginController.cs
+++ MyAbpProject.DcTms/Areas/Admin/Controllers/LoginController.cs
@@ -2,17 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Abp.Domain.Uow;
 using Microsoft.AspNetCore.Mvc;
+using MyAbpProject.AdminLogin;
+using MyAbpProject.AdminLogin.Dtos;
 using MyAbpProject.Controllers;
+using MyAbpProject.Identity;
 
 namespace MyAbpProject.DcTms.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class LoginController : MyAbpProjectControllerBase
     {
+        private readonly IManagerService _manager;
+        private readonly SignInManager _signInManager;
+        public LoginController(IManagerService manager
+            , SignInManager signInManager)
+        {
+            _manager = manager;
+            _signInManager = signInManager;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [UnitOfWork]
+        public virtual async Task<JsonResult> CheckLogin(VmAdminLogin vm, string returnUrl = "", string returnUrlHash = "")
+        {
+            try
+            {
+                var loginResult = await _manager.CheckLogin(vm);
+                if (null == loginResult)
+                {
+                    return ResponseFail("系统繁忙，请稍后重试！");
+                }
+                if (loginResult.Success)
+                {
+                    await _signInManager.SignOutAndSignInAsync(loginResult.Identity, true);
+                    return ResponseSucc("登录成功", new { returnUrl = returnUrl });
+                }
+                return ResponseFail(loginResult.Message);
+            }
+            catch (Exception ex)
+            {
+                return ResponseException();
+            }
         }
     }
 }
--- MyAbpProject.DcTms/wwwroot/view-resources/Admin/Views/Login/login.js
+++ MyAbpProject.DcTms/wwwroot/view-resources/Admin/Views/Login/login.js
@@ -1,6 +1,52 @@
 ﻿/// <reference path="../../../../lib/jquery/dist/jquery.js" />
 $(document).ready(function () {
-    $('#btnSubmit').click(function () {
-        alert('true');
+    //$('#btnSubmit').click(function () {
+    //    alert('true');
+    //});
+    var $loginForm = $('#form1');
+    $loginForm.validate({
+        highlight: function (input) {
+            //$(input).parents('.form-line').addClass('error');
+            $('.login-tips').show();
+        },
+        unhighlight: function (input) {
+            //$(input).parents('.form-line').removeClass('error');
+            console.log('unhighlight');
+            $('.login-tips').hide();
+        },
+        errorPlacement: function (error, element) {
+            //$(element).parents('.input-group').append(error);
+            console.log('errorPlacement');
+            $('.login-tips').show();
+        }
+    });
+    $loginForm.submit(function (e) {
+        e.preventDefault();
+
+        if (!$loginForm.valid()) {
+            return;
+        }
+
+        abp.ui.setBusy(
+            $('.loginbody'),
+
+            abp.ajax({
+                contentType: 'application/x-www-form-urlencoded',
+                url: $loginForm.attr('action'),
+                data: $loginForm.serialize()
+            }).done(function (data) {
+                //abp.notify.success('created new person with id = ' + data.personId);
+                if (data.code == 0) {
+                    abp.notify.success(data.msg);
+                    if (data.data.returnUrl) {
+                        window.location.href = data.data.returnUrl;
+                    } else {
+                        window.location.href = '/admin/adminhome/index';
+                    }
+                } else {
+                    abp.notify.warn(data.code + '： ' + data.msg, '登录提示');
+                }
+            })
+        );
     });
 });
