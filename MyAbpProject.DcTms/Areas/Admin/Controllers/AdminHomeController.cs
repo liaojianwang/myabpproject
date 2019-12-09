1447bdad55f1748c3ad6b3db7fedaed4a44a8226
liaojianwang <liaojianwang@qibingdaojia.com>
2019-12-09 18:03:38 +0800

    新增后台主页。

---
+++ MyAbpProject.DcTms/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Abp.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyAbpProject.Controllers;
+
+namespace MyAbpProject.DcTms.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AbpMvcAuthorize]
+    public class AdminHomeController : MyAbpProjectControllerBase
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
